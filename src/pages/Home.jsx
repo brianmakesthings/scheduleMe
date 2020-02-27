c3d48f5ff101c787ebd2c65367386598520eba88
Anthony Pham <khanhdinhpham@gmail.com>
2020-02-27 14:46:11 -0800

    Add: Home nested routing test

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,28 @@
-import React from 'react';
+import React from "react";
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	Link,
+	useHistory
+} from "react-router-dom";
 
-function Home() {
-  const buttonTest = () => {
-    alert('Ohayo');
-  };
-  
-  return (
-    <button onClick={buttonTest}>
-      Create survey
-    </button>
-  );
-}
+import Input from "../pages/Input";
+
+const Home = props => {
+	let history = useHistory();
+	return (
+		<div>
+			<button onClick={() => history.push("/input")}>
+				Create Survey
+			</button>
+
+			<Link to={"/input"}></Link>
+			<Switch>
+				<Route path={"/input"} exact component={Input} />
+			</Switch>
+		</div>
+	);
+};
 
 export default Home;
