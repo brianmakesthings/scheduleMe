f219018bf79eb4a0e774e4449081ef036f504851
Moe_Skeeto <helhatty@sfu.ca>
2020-03-03 15:46:30 -0800

    Added delete function for db

--- controllers/timeSheet-ctrl.js
+++ controllers/timeSheet-ctrl.js
@@ -72,3 +72,19 @@ updatetimeSheet = async (req, res) => {
             })
     })
 }
+
+deletetimeSheet = async (req, res) => {
+    await timeSheet.findOneAndDelete({ _id: req.params.id }, (err, timeSheet) => {
+        if (err) {
+            return res.status(400).json({ success: false, error: err })
+        }
+
+        if (!timeSheet) {
+            return res
+                .status(404)
+                .json({ success: false, error: `TimeSheet not found` })
+        }
+
+        return res.status(200).json({ success: true, data: timeSheet })
+    }).catch(err => console.log(err))
+}
