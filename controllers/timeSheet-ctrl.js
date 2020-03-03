a5ab372fdb8f418e9639872a90c176b4689b8972
Moe_Skeeto <helhatty@sfu.ca>
2020-03-03 15:38:12 -0800

    Updated timesheet db to include update function

--- controllers/timeSheet-ctrl.js
+++ controllers/timeSheet-ctrl.js
@@ -28,7 +28,47 @@ createTimeSheet = (req, res) => {
        .catch(error => {
            return res.status(400).json({
                error,
-               message: 'Time Sheet Created',
+               message: 'Time Sheet Not Created',
            })
        })
+}
+
+updatetimeSheet = async (req, res) => {
+    const body = req.body
+
+    if (!body) {
+        return res.status(400).json({
+            success: false,
+            error: 'You must provide a body to update!',
+        })
+    }
+
+    timeSheet.findOne({ _id: req.params.id }, (err, timeSheet) => {
+        if (err) {
+            return res.status(404).json({
+                err,
+                message: 'TimeSheet not found!',
+            })
+        }
+        timeSheet.title = body.title
+        timeSheet.dates = body.dates
+        timeSheet.timeStart = body.timeStart
+        timeSheet.timeEnd = body.timeEnd
+        timeSheet.increments = body.increments
+        timeSheet
+            .save()
+            .then(() => {
+                return res.status(200).json({
+                    success: true,
+                    id: timeSheet._id,
+                    message: 'timeSheet updated!',
+                })
+            })
+            .catch(error => {
+                return res.status(404).json({
+                    error,
+                    message: 'timeSheet not updated!',
+                })
+            })
+    })
 }
