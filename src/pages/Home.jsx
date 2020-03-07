ab8bae403a22b5ef1b2f631911b63b8c63bb498a
Anthony Pham <khanhdinhpham@gmail.com>
2020-03-06 20:24:43 -0800

    Update: Add form components to Input page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,56 +1,17 @@
 import React from "react";
 
-import { useForm, useStep } from "react-hooks-helper";
-
-import Names from "../components/Forms/Names/Names";
-import Contact from "../components/Forms/Contact/Contact";
-import Time from "../components/Forms/Time/Time";
-import Review from "../components/Forms/Review/Review";
-import Submit from "../components/Forms/Submit/Submit";
-
-const steps = [
-	{ id: "names" },
-	{ id: "contact" },
-	{ id: "time" },
-	{ id: "review" },
-	{ id: "submit" }
-];
-
-const defaultData = {
-	firstName: "Anthony",
-	lastName: "Pham",
-	nickName: "AP",
-	email: "email@domain.com",
-	phone: "+61 4252 454 332",
-	timeBegin: "2020-03-01T23:28:56.782Z",
-	timeEnd: "2014-01-01T23:28:56.782Z"
-};
+import { useHistory } from "react-router-dom";
 
 const Home = ({ images }) => {
-	const [formData, setForm] = useForm(defaultData);
-	const { step, navigation } = useStep({ initialStep: 0, steps });
-	const { id } = step;
+	let history = useHistory();
 
-	const props = { formData, setForm, navigation };
-
-	switch (id) {
-		case "names":
-			return <Names {...props} />;
-		case "contact":
-			return <Contact {...props} />;
-		case "time":
-			return <Time {...props} />;
-		case "review":
-			return <Review {...props} />;
-		case "submit":
-			return <Submit {...props} />;
-		default:
-			return null;
-	}
+	const handleClick = () => {
+		history.push("/input");
+	};
 
 	return (
 		<div>
-			<p>Home</p>
+			<button onClick={handleClick}>Create Survey</button>
 		</div>
 	);
 };
--- src/pages/Input.jsx
+++ src/pages/Input.jsx
@@ -1,28 +1,56 @@
 import React from "react";
-import { useInput } from "../hooks/input-hook";
 
-const Input = props => {
-	const { value, bind, reset } = useInput("");
+import { useForm, useStep } from "react-hooks-helper";
 
-	const handleSubmit = event => {
-		event.preventDefault();
-		alert(`Submitting Name ${value}`);
-		reset();
-	};
+import Names from "../components/Forms/Names/Names";
+import Contact from "../components/Forms/Contact/Contact";
+import Time from "../components/Forms/Time/Time";
+import Review from "../components/Forms/Review/Review";
+import Submit from "../components/Forms/Submit/Submit";
+
+const steps = [
+	{ id: "names" },
+	{ id: "contact" },
+	{ id: "time" },
+	{ id: "review" },
+	{ id: "submit" }
+];
+
+const defaultData = {
+	firstName: "Anthony",
+	lastName: "Pham",
+	nickName: "AP",
+	email: "email@domain.com",
+	phone: "+61 4252 454 332",
+	timeBegin: "2020-03-01T23:28:56.782Z",
+	timeEnd: "2014-01-01T23:28:56.782Z"
+};
+
+const Input = ({ images }) => {
+	const [formData, setForm] = useForm(defaultData);
+	const { step, navigation } = useStep({ initialStep: 0, steps });
+	const { id } = step;
+
+	const props = { formData, setForm, navigation };
+
+	switch (id) {
+		case "names":
+			return <Names {...props} />;
+		case "contact":
+			return <Contact {...props} />;
+		case "time":
+			return <Time {...props} />;
+		case "review":
+			return <Review {...props} />;
+		case "submit":
+			return <Submit {...props} />;
+		default:
+			return null;
+	}
 
 	return (
 		<div>
-			<form onSubmit={handleSubmit}>
-				<label>
-					Name:
-					<input type="text" {...bind} />
-				</label>
-				<input type="submit" value="Submit" />
-			</form>
-
-			<div className="display">
-				<p>Name {value}</p>
-			</div>
+			<p>Home</p>
 		</div>
 	);
 };
