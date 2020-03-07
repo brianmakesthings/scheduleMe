695c7a00530ce696fab6e208ba067814b1beff3a
Anthony Pham <khanhdinhpham@gmail.com>
2020-03-06 19:17:18 -0800

    Add: Multi-level form

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,26 +1,56 @@
 import React from "react";
-import {
-	BrowserRouter as Router,
-	Switch,
-	Route,
-	Link,
-	useHistory
-} from "react-router-dom";
 
-import Input from "../pages/Input";
+import { useForm, useStep } from "react-hooks-helper";
 
-const Home = props => {
-	let history = useHistory();
+import Names from "../components/Forms/Names/Names";
+import Contact from "../components/Forms/Contact/Contact";
+import Time from "../components/Forms/Time/Time";
+import Review from "../components/Forms/Review/Review";
+import Submit from "../components/Forms/Submit/Submit";
+
+const steps = [
+	{ id: "names" },
+	{ id: "contact" },
+	{ id: "time" },
+	{ id: "review" },
+	{ id: "submit" }
+];
+
+const defaultData = {
+	firstName: "Anthony",
+	lastName: "Pham",
+	nickName: "AP",
+	email: "email@domain.com",
+	phone: "+61 4252 454 332",
+	timeBegin: "2020-03-01T23:28:56.782Z",
+	timeEnd: "2014-01-01T23:28:56.782Z"
+};
+
+const Home = ({ images }) => {
+	const [formData, setForm] = useForm(defaultData);
+	const { step, navigation } = useStep({ initialStep: 0, steps });
+	const { id } = step;
+
+	const props = { formData, setForm, navigation };
+
+	switch (id) {
+		case "names":
+			return <Names {...props} />;
+		case "contact":
+			return <Contact {...props} />;
+		case "time":
+			return <Time {...props} />;
+		case "review":
+			return <Review {...props} />;
+		case "submit":
+			return <Submit {...props} />;
+		default:
+			return null;
+	}
+
 	return (
 		<div>
-			<button onClick={() => history.push("/input")}>
-				Create Survey
-			</button>
-
-			<Link to={"/input"}></Link>
-			<Switch>
-				<Route path={"/input"} exact component={Input} />
-			</Switch>
+			<p>Home</p>
 		</div>
 	);
 };
