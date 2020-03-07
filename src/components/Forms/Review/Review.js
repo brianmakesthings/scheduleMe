ae73ac4565db47dd660f5ccf9ab858c08a46d148
Anthony Pham <khanhdinhpham@gmail.com>
2020-03-07 14:17:48 -0800

    Add: Date Input Review

--- src/components/Forms/Review/Review.js
+++ src/components/Forms/Review/Review.js
@@ -4,17 +4,19 @@ const Review = ({ setForm, formData, navigation }) => {
 	const {
 		firstName,
 		lastName,
-		nickName,
 		email,
-		phone,
+		dateBegin,
+		dateEnd,
 		timeBegin,
 		timeEnd
 	} = formData;
+
 	const { go } = navigation;
 
 	return (
 		<div className="form">
 			<h3>Review your data</h3>
+			{/* Name Review */}
 			<h4>
 				Name
 				<button onClick={() => go("names")}>Edit</button>
@@ -25,25 +27,36 @@ const Review = ({ setForm, formData, navigation }) => {
 				<br />
 				Last Name: {`${lastName}`},
 			</div>
-			<div>Nick Name: {`${nickName}`}</div>
+
+			{/* Contact review */}
 			<h4>
 				Contact
 				<button onClick={() => go("contact")}>Edit</button>
 			</h4>
+			<div>E-mail: {`${email}`}</div>
+
+			{/* Date Input review */}
+			<h4>
+				Date Input
+				<button onClick={() => go("date")}>Edit</button>
+			</h4>
 			<div>
-				Phone: {`${phone}`},
+				{" "}
+				Date Begin: {`${dateBegin}`},
 				<br />
-				E-mail: {`${email}`}
+				Date End: {`${dateEnd}`},
 			</div>
+
+			{/* Time Input Review */}
 			<h4>
 				Time Input
-				<button onClick={() => go("names")}>Edit</button>
+				<button onClick={() => go("time")}>Edit</button>
 			</h4>
 			<div>
 				{" "}
 				Time Begin: {`${timeBegin}`},
 				<br />
-				TimeEnd: {`${timeEnd}`},
+				Time End: {`${timeEnd}`},
 			</div>
 			<div>
 				<button onClick={() => go("submit")}>Submit</button>
